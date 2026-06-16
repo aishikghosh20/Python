aa5a31952c36bd676e61d0c49d03df5503c27086
Aishik Ghosh <aishikghosh14@gmail.com>
2026-06-16 19:52:56 +0530

    Basics of Variables and Datatypes

---
+++ BASICS/variables.py
@@ -0,0 +1,14 @@
+a=6 # a is an integer
+
+b=5.22 # b is floating-point
+
+print("product :", a+b)
+
+name = "Aishik"  # this is a string
+print("Name:", name)
+
+check = False # boolean datatype
+
+empty = None # None datatype
+
+
