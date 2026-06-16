4a2b109b5ed605fab1fdf65e59d0648193eecab1
Aishik Ghosh <aishikghosh14@gmail.com>
2026-06-17 02:37:32 +0530

    Basics of the operators

---
+++ BASICS/operators.py
@@ -0,0 +1,33 @@
+# arithmatic operators
+a=7
+b=5
+c= a+b 
+print(c)
+
+# Assignment Operators
+a =4-2
+b=6
+b += 3 # Increments the value of b by 3
+
+print(b)
+
+
+# Comparision Operator
+d= 5<4
+print(d)
+
+e = b == a
+print (e)
+
+
+# Logical Operators
+e = True or False 
+print (e)
+
+f = True and False
+print (f)
+
+print(not(e)) # Reverses the boolean output
+print (not(False))
+
+
