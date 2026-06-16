af9d5bbecdab5e93b7e9727504d374b54d592cfa
Aishik Ghosh <aishikghosh14@gmail.com>
2026-06-17 02:51:41 +0530

    Basics of input() function

---
+++ BASICS/user_input.py
@@ -0,0 +1,15 @@
+a = input("Enter the 1st no : ") # Take input as a string
+b = input ("Enter the 2nd no : ")
+
+print("The the 1st no s :", a)
+print("The the 2nd no s :", b)
+
+print("The sum is : ", a+b) # Concanates the 2 strings
+
+
+# to actually sum the inputs
+a = int(input("Enter the 1st no : ")) 
+b = int(input("Enter the 2nd no : ")) 
+
+print("The sum is : ", a+b)
+
