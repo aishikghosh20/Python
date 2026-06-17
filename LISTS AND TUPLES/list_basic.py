c048daf2c444bc7e2521e2c953c3b7b6fd082085
Aishik Ghosh <aishikghosh14@gmail.com>
2026-06-17 21:55:30 +0530

    Understanding the basics of lists

---
+++ LISTS AND TUPLES/list_basic.py
@@ -0,0 +1,11 @@
+data = ["Aishik", "Jojo", 6, 67.06, True, 'A', None] # this is a list 
+print(data[3],data[4])
+
+# Altering the elements - lists are mutable
+data[3] = False
+print(data[3],data[4])
+
+# Slicing 
+print(data[1:4])
+
+
