9884400868575845a3d541b69a19b9138d6883c5
Aishik Ghosh <aishikghosh14@gmail.com>
2026-06-17 20:24:24 +0530

    Demonstarting the use of escape sequence characters with examples

---
+++ BASICS/escape_sequences.py
@@ -0,0 +1,13 @@
+# Newline Char - '\n'
+a= "My name is Aishik. \nI am studying at St.Xaviers University\nI like coding"
+print(a)
+print("\n")
+
+# for Quotations inside a quote
+print("Harry said, \"He is happy\"")
+print('Harry said, "He is happy"')
+print("Harry said, 'He is happy'")
+print("\n")
+
+# To print a backlash
+print("Harry\\Bill")
