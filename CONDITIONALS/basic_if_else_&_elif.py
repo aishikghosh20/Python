b4cd92f6655ffdcf31cf05d8c414b44f27461785
Aishik Ghosh <aishikghosh14@gmail.com>
2026-06-19 11:42:20 +0530

    Basics in if-elif-else ladder

---
+++ CONDITIONALS/basic_if_else_&_elif.py
@@ -0,0 +1,9 @@
+a = int(input("Enter your age:\n"))
+
+if (a >= 18):
+    print("You are above the age of consent")
+elif (a<=0):
+    print("Invalid Age Entered")
+else :
+    print("You are beloqw the age of consent")
+
