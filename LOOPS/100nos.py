e8293dece801017c55037d07dd32d2ad1a00c704
Aishik Ghosh <aishikghosh14@gmail.com>
2026-06-19 13:12:37 +0530

    BAsics of while-loop

--- LOOPS/100nos.py
+++ LOOPS/100nos.py
@@ -1,5 +1,5 @@
-for i in range(50):
+for i in range(50): # starts with 0 and ends before the specified no
     print(i)
 
-for i in range(51,101):
+for i in range(51,101): # starts with 51 and ends before 101
     print(i)
---
+++ LOOPS/sum_n_nos.py
@@ -0,0 +1,11 @@
+n =int(input("Enter n:\n"))
+sum=0
+i=1
+while(i<=n):
+    sum+=i
+    i+=1
+
+print(sum)
+
+
+
