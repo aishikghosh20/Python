4ac9be6cba7dd09eec776064d965684318d3777b
Aishik Ghosh <aishikghosh14@gmail.com>
2026-06-24 11:43:39 +0530

    Basics in OOPs

---
+++ OBJECT ORIENTED/class_basics.py
@@ -0,0 +1,8 @@
+class employee: # claass attributes 
+    language= "Hindi"
+    salary = 120000
+
+emp1 = employee()
+emp1.name = "harry" # object attribute , will not affect the class
+print(emp1.name,emp1.language,emp1.salary )
+
